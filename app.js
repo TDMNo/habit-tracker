eed73240c6f68df4ee967c7aac7bc74983ff2333
TDMNo <tdm.noy@gmail.com>
2025-06-24 15:19:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -207,8 +207,6 @@ const confirmNo = document.getElementById('confirm-no');
 deleteUserBtn.onclick = () => {
   if (!userName) return;
 
-  const confirmDelete = confirm(`Удалить участника "${userName}"? Это действие необратимо.`);
-  if (!confirmDelete) return;
   confirmText.textContent = `Удалить участника "${userName}"?`;
   confirmModal.classList.remove('hidden');
 
